72e0cb5505539c5daef481a22844288b084085b7
Bradzie <70105933+Bradzie@users.noreply.github.com>
2022-06-06 10:13:02 +0100

    Add files via upload
    First commit!

---
+++ Driftout/client/js/sketch.js
@@ -0,0 +1,15 @@
+
+// Load prior to game start
+function preload(){
+  //something
+}
+
+// Called when game is started once
+function setup(){
+  console.log("hello world");
+}
+
+// Called within a loop for drawing to canvas (no rendering)
+function draw(){
+  console.log("loop");
+}
---
+++ Driftout/server/server.js
@@ -0,0 +1,4 @@
+var path = require("path");
+var http = require("http");
+var express = require("express");
+var socketIO = require("socket.io");
