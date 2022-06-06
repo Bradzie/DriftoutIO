1a410c307a2dad9bf4328b909fefc230e4fdc06d
Bradzie <70105933+Bradzie@users.noreply.github.com>
2022-06-06 11:28:24 +0100

    Socket io connection and basic server

--- Driftout/server/server.js
+++ Driftout/server/server.js
@@ -2,3 +2,22 @@ var path = require("path");
 var http = require("http");
 var express = require("express");
 var socketIO = require("socket.io");
+
+// Needs replacement upon cloud hosting?
+var publicPath = path.join(__dirname, "../client")
+var port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+app.use(express.static(publicPath))
+console.log("Server Started!")
+
+server.listen(port, function(){
+  console.log("Server started on port " + port);
+});
+
+io.on("connection", function(socket){
+  socket.on("message", function(data){
+    console.log(data);
+  });
+});
