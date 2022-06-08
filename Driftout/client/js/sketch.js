498da08e0361099f17207c420488015b217d7de7
Bradzie <bradgeary@rocketmail.com>
2022-06-08 20:54:28 +0100

    Added translate function and movement events to a new player object. Interaction between player object draw function and main draw function does not work ):

--- Driftout/client/js/sketch.js
+++ Driftout/client/js/sketch.js
@@ -1,4 +1,6 @@
 var socket;
+var player1;
+var player2;
 
 // Load prior to game start
 function preload(){
@@ -12,46 +14,66 @@ function setup(){
   socket.on("returnMessage", function(data){
     console.log(data);
   })
+  player1 = Player('Brad', 100, 100);
+  player2 = Player('Chloe', 200, 200);
 
   createCanvas(windowWidth, windowHeight);
-  background(50, 50, 50);
-
-  // x, y, w, h
-//  for (let y = 20; y < windowHeight; y=y+75){
-//    for (let x = 20; x < windowWidth; x=x+75){
-//      rect (x, y, 55, 55)
-//    }
-//  }
-
-//triangle(x+25, y, x-25, y+15, x-25, y-15);
 
 }
 
-var x = 600;
-var y = 200;
-
-var velocityX = 10;
-var velocityY = 0;
-
-var gravity = 0.2;
-
-
-
 // Called within a loop for drawing to canvas (no rendering)
 function draw(){
-  resizeCanvas(windowWidth, windowHeight)
   background(50, 50, 50);
+  translate(width/2 - player1.x, height/2 - player1.y);
+
   fill(100)
-  circle(x, y, 40)
-  x = x + velocityX;
-  y = y + velocityY;
-  velocityY+=gravity;
-  //velocityX+=gravity;
-  if (y > windowHeight || y < 0){
-    velocityY = -velocityY;
+  rect(300, 300, 600, 600);
+
+  // movement
+  if (keyIsDown(65)){
+    if (player1.vX > -4){
+      player1.vX = player1.vX - 0.1;
+    }
   }
-  if (x > windowWidth || x < 0){
-    velocityX = -velocityX;
+  if (keyIsDown(68)){
+    if (player1.vX < 4){
+      player1.vX = player1.vX + 0.1;
+    }
+  }
+  if (keyIsDown(87)){
+    if (player1.vY > -4){
+      player1.vY = player1.vY - 0.1;
+    }
+  }
+  if (keyIsDown(83)){
+    if (player1.vY < 4){
+      player1.vY = player1.vY + 0.1;
+    }
   }
 
+  player1.draw();
+  player2.draw();
+
+}
+
+var Player = function(name, x, y){
+  this.name = name;
+  this.x = x;
+  this.y = y;
+  this.vX = 0;
+  this.vY = 0;
+
+  this.draw = function(){
+
+    this.x += this.vX;
+    this.y += this.vY;
+
+    player1.vX = this.vX / 1.01;
+    player1.vY = this.vY / 1.01;
+
+    fill(150);
+    circle(this.x, this.y, 40)
+  }
+
+  return this;
 }
