3637750418b0281a75516c7f2f78a88612bd6361
Bradzie <bradgeary@rocketmail.com>
2022-06-09 09:20:15 +0100

    Player object now functional, extra players now render to the screen.

--- Driftout/client/js/sketch.js
+++ Driftout/client/js/sketch.js
@@ -14,66 +14,71 @@ function setup(){
   socket.on("returnMessage", function(data){
     console.log(data);
   })
-  player1 = Player('Brad', 100, 100);
-  player2 = Player('Chloe', 200, 200);
+  player1 = new Player('Brad', 0, 50);
+  player2 = new Player('Chloe', 450, 400);
 
   createCanvas(windowWidth, windowHeight);
 
 }
 
-// Called within a loop for drawing to canvas (no rendering)
-function draw(){
-  background(50, 50, 50);
-  translate(width/2 - player1.x, height/2 - player1.y);
+// this is called alot of times per second (FPS, frame per second)
+function draw() {
+    background(200, 200, 200); // it gets a hex/rgb color
+    translate(width/2 - player1.x, height/2 - player1.y);
 
-  fill(100)
-  rect(300, 300, 600, 600);
+    fill(100);
+    rect(300, 300, 600, 600);
 
-  // movement
-  if (keyIsDown(65)){
-    if (player1.vX > -4){
-      player1.vX = player1.vX - 0.1;
-    }
-  }
-  if (keyIsDown(68)){
-    if (player1.vX < 4){
-      player1.vX = player1.vX + 0.1;
-    }
-  }
+    movement();
+
+    player1.draw();
+    player2.draw();
+}
+
+function movement(){
   if (keyIsDown(87)){
-    if (player1.vY > -4){
-      player1.vY = player1.vY - 0.1;
+    if(player1.vY > -5){
+      player1.vY += -0.2;
     }
   }
   if (keyIsDown(83)){
-    if (player1.vY < 4){
-      player1.vY = player1.vY + 0.1;
+    if(player1.vY < 5){
+      player1.vY += 0.2;
     }
   }
-
-  player1.draw();
-  player2.draw();
-
+  if (keyIsDown(65)){
+    if(player1.vX > -5){
+      player1.vX += -0.2;
+    }
+  }
+  if (keyIsDown(68)){
+    if(player1.vX < 5){
+      player1.vX += 0.2;
+    }
+  }
 }
 
-var Player = function(name, x, y){
-  this.name = name;
-  this.x = x;
-  this.y = y;
-  this.vX = 0;
-  this.vY = 0;
 
-  this.draw = function(){
+// The player object constructor
+var Player = function(name, x, y) {
+    this.name = name;
+    this.x = x;
+    this.y = y;
+    this.vX = 0;
+    this.vY = 0;
 
-    this.x += this.vX;
-    this.y += this.vY;
+    this.draw = function() {
 
-    player1.vX = this.vX / 1.01;
-    player1.vY = this.vY / 1.01;
+        fill(60);
+        circle(this.x, this.y, 40);
 
-    fill(150);
-    circle(this.x, this.y, 40)
-  }
+        this.x += this.vX;
+        this.y += this.vY;
 
-  return this;
+        this.vX = this.vX * 0.99;
+        this.vY = this.vY * 0.99;
+
+    }
+
+    return this;
 }
