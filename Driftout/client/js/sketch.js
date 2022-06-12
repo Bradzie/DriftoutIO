a26331b966d935114e031fc483ede31c5ed7017c
Bradzie <bradgeary@rocketmail.com>
2022-06-12 12:30:58 +0100

    Health bars and collision damage for inside wall

--- Driftout/client/js/sketch.js
+++ Driftout/client/js/sketch.js
@@ -1,19 +1,24 @@
 var socket;
+
+// Constants
+
 var player1;
 var player2;
 var currentCar;
 var allCars;
+var boostCooldown = 1000;
+var canBoost = 0;
 
 // Load prior to game start
 function preload(){
   allCars = {
-    racer : new Car('Racer', 150, 6, 8, []),
-    prankster : new Car('Prankster', 120, 6, 5, []),
-    bullet : new Car('Bullet', 100, 10, 5, []),
-    spike : new Car('Spike', 150, 5, 3, []),
-    tank : new Car('Tank', 200, 4, 5, []),
-    sprinter : new Car('Sprinter', 80, 12, 10, []),
-    fragile : new Car('Fragile', 70, 6, 5, [])
+    racer : new Car('Racer', 150, 6, 8, [], 0.1),
+    prankster : new Car('Prankster', 120, 6, 5, [], 0.1),
+    bullet : new Car('Bullet', 100, 10, 5, [], 0.08),
+    spike : new Car('Spike', 150, 5, 3, [], 0.12),
+    tank : new Car('Tank', 200, 4, 5, [], 0.15),
+    sprinter : new Car('Sprinter', 80, 12, 10, [], 0.1),
+    fragile : new Car('Fragile', 70, 6, 5, [], 0.1)
   };
 }
 
@@ -25,8 +30,8 @@ function setup(){
     console.log(data);
   })
   currentCar = new Car('Racer', 150, 6, 8, []);
-  player1 = new Player('Brad', -50, 1000, "tank");
-  player2 = new Player('Chloe', 50, 1000, "racer");
+  player1 = new Player('Brad', -50, 1000, allCars.tank);
+  player2 = new Player('Chloe', 50, 1000, allCars.racer);
 
   console.log(allCars.tank);
 
@@ -50,6 +55,11 @@ function draw() {
 }
 
 function movement(){
+  if (mouseIsPressed == true && millis() > canBoost){
+    player1.vX *= 2;
+    player1.vY *= 2;
+    canBoost = millis() + boostCooldown;
+  }
   if (keyIsDown(87)){
     if(player1.vY > -player1.maxSpeed){
       player1.vY += -player1.acceleration;
@@ -70,6 +80,8 @@ function movement(){
       player1.vX += player1.acceleration;
     }
   }
+  // player1.vX *= 0.995;
+  // player1.vY *= 0.995;
 }
 
 function doCollisions(){
@@ -77,6 +89,7 @@ function doCollisions(){
   // Inside rect
   if ((player1.x > 200 && player1.x < 225) && (player1.y > 200 && player1.y < 1600)){
     player1.x -= 1;
+    player1.HP -= player1.vX*4;
     player1.vX = -player1.vX * 0.7;
   }
 
@@ -143,22 +156,24 @@ function drawMap(){
 
 // The player object constructor
 var Player = function(name, x, y, car) {
-  console.log(allCars[car]);
+  console.log(car);
   this.name = name;
   this.x = x;
   this.y = y;
   this.vX = 0;
   this.vY = 0;
-  this.car = allCars[car];
-  this.maxHP = allCars[car].maxHP;
-  this.maxSpeed = allCars[car].maxSpeed;
-  this.maxBoosts = allCars[car].maxBoosts;
-  this.acceleration = allCars[car].acceleration;
+  this.car = car;
+  this.maxHP = car.maxHP;
+  this.HP = car.maxHP;
+  this.maxSpeed = car.maxSpeed;
+  this.maxBoosts = car.maxBoosts;
+  this.acceleration = car.acceleration;
 
   this.draw = function() {
     var angle = atan2(mouseY - windowHeight/2, mouseX - windowWidth/2);
     // decide angle of mouse cursor from middle of canvas
 
+    // Player's car
     push();
     fill(60);
     translate(this.x, this.y);
@@ -169,6 +184,22 @@ var Player = function(name, x, y, car) {
     vertex(-25, 20);
     vertex(-25, -20);
     endShape(CLOSE);
+    pop();
+
+    // Player's name
+    textSize(30);
+    textAlign(CENTER);
+    textStyle(BOLD);
+    text(this.name + " " + this.HP, this.x, this.y + 60);
+
+    // Player's health
+    push();
+    strokeWeight(5);
+    stroke(220, 0, 0);
+    line(this.x - 40, this.y + 70, this.x + 40, this.y + 70);
+    stroke(0, 220, 0);
+    line(this.x - (this.HP / (this.HP / 40)), this.y + 70,
+         this.x + (this.HP / (this.HP / 40)), this.y + 70);
     pop();
 
     this.x += this.vX;
@@ -182,11 +213,11 @@ var Player = function(name, x, y, car) {
 }
 
 // The car object constructor
-var Car = function(name, maxHP, maxSpeed, maxBoosts, upgrades){
+var Car = function(name, maxHP, maxSpeed, maxBoosts, upgrades, acceleration){
   this.name = name;
   this.maxHP = maxHP;
   this.maxSpeed = maxSpeed;
   this.maxBoosts = maxBoosts;
   this.upgrades = upgrades;
-  this.acceleration = 0.1;
+  this.acceleration = acceleration;
 }
