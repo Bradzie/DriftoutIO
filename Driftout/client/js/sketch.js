12b494d74ab3688bbc221849800e35d0ec4647eb
Bradzie <bradgeary@rocketmail.com>
2022-09-22 16:58:32 +0100

    Redesigned menu, mobile functionality

--- Driftout/client/js/sketch.js
+++ Driftout/client/js/sketch.js
@@ -74,6 +74,7 @@ var totalConnections=0;
 var playerNames = [];
 var currentRoom = null;
 var myName;
+var isMobile;
 
 function preload(){
 }
@@ -94,6 +95,8 @@ function setup(){
   menuContainer.style.opacity = "1";
 
   classDisplay.innerHTML = "<div id='classImage'><img src = " + classAssetPaths[classIndex] + "></div>" + classEntries[classIndex];
+
+  console.log(isMobile);
 
   socket = io();
 
@@ -252,7 +255,7 @@ function setup(){
 function draw() {
   resizeCanvas(windowWidth, windowHeight);
   if (playing == true && allPlayers.filter(player => player.id === myId).length == 1){
-    background(100, 100, 100); // it gets a hex/rgb color
+    background(37, 150, 190); // it gets a hex/rgb color
     sendInputData();
     refreshDisplays();
 
@@ -274,6 +277,10 @@ function draw() {
       }
     }
 
+    if(isMobile == true){
+      drawMobileControls();
+    }
+
     for(var i in allPlayers) {
       if(allPlayers[i].alive == true){
         allPlayers[i].draw();
@@ -285,10 +292,30 @@ function draw() {
       }
     }
 
-
     if(allPlayers.filter(player => player.id === myId).length == 0){
       exitGame();
     }
+  }
+  else{
+  }
+}
+
+function mobileCheck(){
+  let check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
+
+var isMobile = mobileCheck()? true : false;
+
+function drawMobileControls(){
+  for(var i in allPlayers) {
+      if(allPlayers[i].id == myId) {
+        fill(100);
+        circle(allPlayers[i].x + (windowWidth*0.3),  allPlayers[i].y + (windowHeight*0.3), 150);
+        fill(200);
+        circle(allPlayers[i].x + (windowWidth*0.3),  allPlayers[i].y + (windowHeight*0.3), 50);
+      }
   }
 }
 
@@ -380,10 +407,10 @@ function changeClass(){
 }
 
 function toggleChat(){
-  if (chatDisplay.style.display === "none") {
-    chatDisplay.style.display = "block";
-  } else {
+  if (chatDisplay.style.display === "block") {
     chatDisplay.style.display = "none";
+  } else {
+    chatDisplay.style.display = "block";
   }
 }
 
@@ -594,11 +621,16 @@ function mapLine(x1, y1, x2, y2, colour1 = [0,0,0], colour2 = [220,220,220], thi
 }
 
 function sendInputData() {
-    clientPlayerAngle = atan2(mouseY - windowHeight/2, mouseX - windowWidth/2);
+    if(isMobile == true){
+      clientPlayerAngle = atan2(mouseY - windowHeight*0.8, mouseX - windowWidth*0.8)
+    }
+    else{
+      clientPlayerAngle = atan2(mouseY - windowHeight/2, mouseX - windowWidth/2);
+    }
     var mouseClick = false;
     var spacePressed = false;
     var numPressed = null;
-    if (mouseIsPressed === true){
+    if (mouseIsPressed === true && isMobile == false){
       mouseClick = true;
     }
     if (keyIsDown(49)){
@@ -741,7 +773,7 @@ allTracks = {
       vertex(2000, -200);
       endShape(CLOSE);
 
-      fill(100);
+      fill(37, 150, 190);
       beginShape();
       vertex(200, 200);
       vertex(200, 1600);
@@ -773,7 +805,7 @@ allTracks = {
       vertex(3000, -200);
       endShape(CLOSE);
 
-      fill(100);
+      fill(37, 150, 190);
       beginShape();
       vertex(-600, 200);
       vertex(-600, 600);
@@ -805,7 +837,7 @@ allTracks = {
       vertex(2000, -200);
       endShape(CLOSE);
 
-      fill(100);
+      fill(37, 150, 190);
       beginShape();
       vertex(200, 200);
       vertex(200, 1600);
@@ -822,7 +854,7 @@ allTracks = {
       vertex(400, 2000);
       endShape(CLOSE);
 
-      fill(100);
+      fill(37, 150, 190);
       strokeWeight(0);
       beginShape();
       vertex(800, 800);
