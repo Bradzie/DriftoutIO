eb672013fb38888efb95a201535eff03819af7d2
Bradzie <bradgeary@rocketmail.com>
2022-10-28 11:18:24 +0100

    invalid reference fixed causing many crashes

--- Driftout/server/server.js
+++ Driftout/server/server.js
@@ -478,11 +478,11 @@ var Player = function(id, name, x, y, car, dev) {
             this.vX *= 0.3;
             this.vY *= 0.3;
             this.HP -= rooms[this.myRoom].currentEntities[i].damage;
-            if(allPlayers){
+            if(rooms[this.myRoom].allPlayers){
               if(this.HP < 0 && rooms[this.myRoom].allPlayers.filter(player => player.id == rooms[this.myRoom].currentEntities[i].ownerId).length > 0){
                 rooms[this.myRoom].allPlayers.filter(player => player.id == rooms[this.myRoom].currentEntities[i].ownerId)[0].upgradePoints++;
                 rooms[this.myRoom].allPlayers.filter(player => player.id == rooms[this.myRoom].currentEntities[i].ownerId)[0].kills++;
-                rooms[this.myRoom].notifications.push(allPlayers.filter(player => player.id == rooms[this.myRoom].currentEntities[i].ownerId)[0].name + " crashed " + this.name + "!");
+                rooms[this.myRoom].notifications.push(rooms[this.myRoom].allPlayers.filter(player => player.id == rooms[this.myRoom].currentEntities[i].ownerId)[0].name + " crashed " + this.name + "!");
                 this.alive = false;
               }
               rooms[this.myRoom].currentEntities.splice(i, 1);
@@ -497,7 +497,8 @@ var Player = function(id, name, x, y, car, dev) {
           if (rooms[this.myRoom].allPlayers[i].id != this.id){
 
             // If this player's car overlaps any other player's car
-            if (Math.sqrt(((this.x-rooms[this.myRoom].allPlayers[i].x)**2)+((this.y-rooms[this.myRoom].allPlayers[i].y)**2)) < this.size + rooms[this.myRoom].allPlayers[i].size) && rooms[this.myRoom].allPlayers[i].alive == true{
+            if (Math.sqrt(((this.x-rooms[this.myRoom].allPlayers[i].x)**2)+((this.y-rooms[this.myRoom].allPlayers[i].y)**2))
+             < this.size + rooms[this.myRoom].allPlayers[i].size && rooms[this.myRoom].allPlayers[i].alive == true){
 
               // Physics Calc
 
